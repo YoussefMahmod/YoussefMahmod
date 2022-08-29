c6fbe194adcb88500f8bb39756d1911199b8c287
Youssef Mahmoud <ymahmoud@instabug.com>
2022-08-29 15:19:09 -0400

    test

--- te.js
+++ te.js
@@ -1,1 +1,5 @@
-// comment the following lines for more information about the options you    can pass to the constructor of
+// comment the following lines for more information asd
+'fasd
+falsesd f
+asdfa
+sdf
